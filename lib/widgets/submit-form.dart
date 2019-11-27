8f2fa02847b1595c382721d5b80dbfc18013613d
delacquab <47065398+delacquab@users.noreply.github.com>
2019-11-27 11:33:41 -0200

    Aula do balta.io

---
+++ lib/widgets/submit-form.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
+
+import 'input.widget.dart';
+import 'loading-button.widget.dart';
+
+class SubmitForm extends StatelessWidget {
+  var gasCtrl = new MoneyMaskedTextController();
+  var alcCtrl = new MoneyMaskedTextController();
+  var busy = false;
+  Function submitFunc;
+
+  SubmitForm({
+    @required this.gasCtrl,
+    @required this.alcCtrl,
+    @required this.busy,
+    @required this.submitFunc,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(left: 30, right: 30),
+          child: Input(
+            ctrl: gasCtrl,
+            label: "Gasolina",
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(left: 30, right: 30),
+          child: Input(
+            ctrl: alcCtrl,
+            label: "Álcool",
+          ),
+        ),
+        SizedBox(
+          height: 25,
+        ),
+        LoadingButton(
+          busy: busy,
+          func: submitFunc,
+          text: "CALCULAR",
+          invert: false,
+        ),
+      ],
+    );
+  }
+}
